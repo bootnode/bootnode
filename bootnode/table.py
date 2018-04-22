e8ada8674bb34461f6c62a9fbb99b720dd9f5a38
Zach Kelling <z@zeekay.io>
2018-04-22 14:00:10 -0500

    Clean up table format code

--- bootnode/table.py
+++ bootnode/table.py
@@ -2,10 +2,20 @@ from tabulate import tabulate
 from collections import Iterable
 
 
+def attr(obj, a):
+    """
+    Returns value of attribute on an object, calling attribute if necessary.
+    """
+    v = getattr(obj, a, None)
+    if callable(v):
+        return v()
+    return v
+
+
 def table(data, *attrs):
     """
-    Takes an object or list of objects and formats listed attributes in a
-    table.
+    Takes an object or list of objects and prints specified attributes in a
+    nicely formatted table.
     """
 
     # Ensure data is iterable
@@ -16,21 +26,7 @@ def table(data, *attrs):
     headers = [a.upper().replace('_', ' ') for a in attrs]
 
     # Generate tabular data from list of objects
-    table = []
-    for obj in data:
-        row = []
-
-        # Grab each attribute off object
-        for a in attrs:
-            v = getattr(obj, a, None)
-
-            # If attribute is callable, use returned value
-            if callable(v):
-                row.append(v())
-            else:
-                row.append(v)
-
-        table.append(row)
+    table   = [[attr(o, a) for a in attrs] for o in data]
 
     # Pretty print table and headers
     print(tabulate(table, headers=headers))
