098f0b6a0aade91dda181f32b2f8fe2773b830e8
David Tai <david@verus.io>
2019-04-24 15:20:08 -0500

    add extra build logging

--- casper/scripts/start.sh
+++ casper/scripts/start.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
+echo "---------- Starting Execution Engine ----------"
 cd /CasperLabs/execution-engine
 $HOME/.cargo/bin/cargo run --bin casperlabs-engine-grpc-server /.casperlabs/.casper-node.sock &
+echo "---------- Start Bootstrapping Node ----------"
 cd /CasperLabs
 ./node/target/universal/stage/bin/casperlabs-node run -s --server-data-dir /.casperlabs &
 while ! [ -s /.casperlabs/genesis/bonds.txt ]; do
     sleep 5
 done
-sleep 10
+sleep 5
+echo "---------- Finish Bootstrapping Node ----------"
 killall -9 java
 sleep 5
+echo "---------- Starting Node ----------"
 KEY_FILE=$(find /.casperlabs -type f -name *.sk | sort | tail -n 1 | head -n 1)
 CL_VALIDATOR_PUBLIC_KEY=$(echo "$KEY_FILE" | awk -F '[/.]' '{print $(NF-1)}')
 CL_VALIDATOR_PRIVATE_KEY=$(cat "$KEY_FILE")
